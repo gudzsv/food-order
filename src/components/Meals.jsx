e67f1653f4ef50e1eb1f67a03ae4a87deb360004
Serhii Hudzenko <gudzsvmail@gmail.com>
2025-01-24 17:23:54 +0100

    Add Error component and add condition lo loading end error in Meals component

--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -1,4 +1,5 @@
 import useHttp from '../hooks/useHttp.js';
+import Error from './Error.jsx';
 import MealItem from './MealItem.jsx';
 
 export default function Meals() {
@@ -9,8 +10,13 @@ export default function Meals() {
 	} = useHttp('http://localhost:3000/meals', null, []);
 
 	if (isLoading) {
-		return <p>loading .....</p>;
+		return <p className='center'>loading .....</p>;
 	}
+
+	if (error) {
+		return <Error title={'Filed to fetch meals'} message={error} />;
+	}
+
 	return (
 		<ul id='meals'>
 			{loadedMeals?.map((meal) => (
